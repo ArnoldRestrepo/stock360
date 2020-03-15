b04e188b05d444ecb291def6e172f476346ba3a4
Arnold Restrepo <restrepo_arnold@outlook.com>
2020-03-15 17:35:25 -0500

    Javascript End

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const slider = document.querySelector("#slider")
-const canvas = document.querySelector("#canvas")
-const url = `https://stockx-360.imgix.net/adidas-yeezy-boost-350-v2-core-black-red_TruView/Images/adidas-yeezy-boost-350-v2-core-black-red_TruView/Lv2/img${number}.jpg?auto=format,compress&q=90&updated_at=1538080256&w=1000`
+const slider = document.querySelector('#slider');
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
+const images = [];
 
-slider.addEventListener("input", handleInputSlider);
+slider.addEventListener('input', handleInputSlider);
+
+window.addEventListener('load', pageLoaded);
+
+function pageLoaded() {
+  for (let i = 1; i <= 26; i++) {
+    const number = i.toString().padStart(2, '00');
+    const url = `https://stockx-360.imgix.net/adidas-yeezy-boost-350-v2-core-black-red_TruView/Images/adidas-yeezy-boost-350-v2-core-black-red_TruView/Lv2/img${number}.jpg?auto=format,compress&q=90&updated_at=1538080256&w=1000`;
+    const image = new Image();
+    image.src = url;
+    image.addEventListener('load', () => {
+      images[i] = image;
+      if (i === 1) {
+        loadImage(i);
+      }
+    });
+  }
+}
+
+function loadImage(index) {
+  ctx.drawImage(images[index], 0, 0, canvas.width, canvas.height);
+}
 
 function handleInputSlider() {
-  console.log(this.value);
+  loadImage(this.value);
 }
